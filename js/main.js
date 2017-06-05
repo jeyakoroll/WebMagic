b5e569ae6ebda305c17fcc1d7045a38ad78d4250
Jenya Korolenko <jeyakoroll@gmail.com>
2017-06-06 01:23:14 +0200

    Finished callback

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 console.log('Hello ');
 console.log('Shenja 3');
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+  let button = $(event.relatedTarget) // Button that triggered the modal
+  let recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  let modal = $(this)
+  modal.find('.modal-title').text('Обратная связь')
+  modal.find('.modal-body input').val(recipient)
+})
