b55c5532692fdd56f27148bef332db563d3edf5d
Jenya Korolenko <jeyakoroll@gmail.com>
2017-06-05 18:51:43 +0200

    Almost finished desktop version

---
+++ README.md
@@ -0,0 +1,14 @@
+
+
+# Must:
+1. install node.js
+2. install bower.js
+3. install gulp.js
+3. install yarn
+
+# For start editing:
+yarn
+*if development*
+gulp dev
+*if prodaction*
+gulp production
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,82 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var pump = require('pump');
+var browserSync = require('browser-sync').create();
+var sourcemaps = require('gulp-sourcemaps');
 var concatCss = require('gulp-concat-css');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
 
-gulp.task('default', function () {
-  return gulp.src('style/*.css')
-    .pipe(concatCss("bundle.css"))
-    .pipe(gulp.dest('out/'));
+var path_js_files = [
+    './js/main.js'
+];
+
+var path_css_files = [
+    './style/main.scss'
+]
+
+gulp.task('dev_compress_js', function (cb) {
+  pump([
+        gulp.src( path_js_files ),
+        sourcemaps.init(),
+        // uglify({'mangle': false}),
+        concat('main.min.js'),
+        sourcemaps.write(),
+        gulp.dest('./dist/js')
+    ],
+    cb
+  );
+});
+
+gulp.task('prod_compress_js', function (cb) {
+  pump([
+        gulp.src( path_js_files ),
+        uglify({'mangle': false}),
+        concat('main.min.js'),
+        gulp.dest('./dist/js')
+    ],
+    cb
+  );
+});
+
+gulp.task('dev_concat_css', function () {
+    return gulp.src(path_css_files)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(concatCss("all.min.css"))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./style/'));
+});
+
+gulp.task('prod_minify_css', function () {
+    gulp.src(path_css_files)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concatCss("all.min.css"))
+        .pipe(cssmin())
+        .pipe(gulp.dest('./style/'));
+});
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir:'./'
+        }
+    });
+});
+gulp.watch(path_js_files,[ 'dev_compress_js']).on('change', function(){
+    setTimeout(function(){
+        browserSync.reload();
+    },200);
+});
+gulp.watch(path_css_files,[ 'dev_concat_css']).on('change', function(){
+    setTimeout(function(){
+        browserSync.reload();
+    },200);
 });
 
 
 
-gulp.task("dev",[]);
-gulp.task("production",[]);
+gulp.task('dev', ['dev_compress_js', 'dev_concat_css', 'browser-sync']);
+gulp.task('production', ['prod_compress_js', 'prod_minify_css']);
