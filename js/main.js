f0c437893811618bbd1b06f6cb9e0c0a9ed24460
Jenya Korolenko <jeyakoroll@gmail.com>
2017-06-07 00:40:17 +0200

    Adjusted the indents of the caps

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-console.log('Hello ');
-console.log('Shenja 3');
-
+// Попап bootstrap
 $('#exampleModal').on('show.bs.modal', function (event) {
   let button = $(event.relatedTarget) // Button that triggered the modal
   let recipient = button.data('whatever') // Extract info from data-* attributes
